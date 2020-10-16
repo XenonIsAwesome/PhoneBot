1e36603ec7ff52bf5dc527987c1ea3bef84d9060
XenonIsAwesome <ofek171202@gmail.com>
2020-10-16 20:35:19 +0300

    Added the setup command without error handeling

--- plugins/commands/settings.py
+++ plugins/commands/settings.py
@@ -7,15 +7,31 @@ class SettingsCommands(Plugin):
     @Plugin.command('setup', '[admin_roles:str...]')
     def on_setup_command(self, event, admin_roles=None):
         if admin_roles:
-            admin_roles = admin_roles.split(' ')
+            admin_roles = admin_roles.replace('<@', '').replace('>', '').split(' ')
 
         # making the unique category and channels for phonebot to use
         try:
             category_id = event.guild.create_category(name='𝙋𝙝𝙤𝙣𝙚𝘽𝙤𝙩').id
             tchannel = event.guild.create_text_channel(name='𝙋𝙝𝙤𝙣𝙚𝘽𝙤𝙩-conversation', parent_id=category_id)
             vchannel = event.guild.create_voice_channel(name='𝙋𝙝𝙤𝙣𝙚𝘽𝙤𝙩 Conversation', parent_id=category_id)
+
+            # adding all the values to the database
+            with TinyDB('phonebot.json') as db:
+                db.update(
+                    {
+                        'text_channel_id': tchannel.id, 
+                        'voice_channel_id': vchannel.id
+                    },
+                    Query().guild_id == event.guild.id
+                )
+
+                if admin_roles:
+                    db.update(
+                        {
+                            'admin_roles': admin_roles
+                        },
+                        Query().guild_id == event.guild.id
+                    )
         except APIException as e:
             event.msg.reply(f'I dont have enough premissions to do that.\n**`Error: {e.args[0]}`**')
-            return
 
-        # adding all the values to the database
