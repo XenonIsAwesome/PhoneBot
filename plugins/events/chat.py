bf1b3e03a654dbf60a8ac48010573cf873a8dd24
XenonIsAwesome <ofek171202@gmail.com>
2020-10-18 21:13:52 +0300

    Added the ability to send messages

--- plugins/events/chat.py
+++ plugins/events/chat.py
@@ -5,17 +5,22 @@ from os import getenv
 
 
 class Chat(Plugin):
-    @Plugin.listen('MessageCreate')
+    @Plugin.listen("MessageCreate")
     def on_message_create(self, event):
-        if event.author.id == getenv("CLIENT_ID"): return
+        if event.author.id == getenv("CLIENT_ID"):
+            return
         if event.guild.id not in [
-                member.id
-                for members in [conv.members for conv in CONVERSATIONS]
+            member.id for members in [conv.members for conv in CONVERSATIONS]
         ]:
             return
-        pass
 
-    @Plugin.listen('MessageReactionAdd')
+        [
+            conv.send(event, event.guild.id)
+            for conv in CONVERSATIONS
+            if event.guild.id in conv.members
+        ]
+
+    @Plugin.listen("MessageReactionAdd")
     def on_reaction_create(self, event):
         wip(event)
         pass
